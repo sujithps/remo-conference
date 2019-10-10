33868a455d9212bafb86a21baf71823938091bd1
Sujith PS <sujithps534@gmail.com>
2019-10-10 22:29:26 +0530

    Sujith | Modify loading icon

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import classNames from 'classnames'
+import loadingIcon from '../../assets/images/refreshing.png'
 import './Loading.scss'
 
+
 export default function Loading({ loading }) {
-    return <div className="AppLoader">
-        { loading ? "Please wait, checking for notification support..." : "" }
+    return <div className="AppLoader" style={ { top: '200px', left: '250px' } }>
+        { loading ?
+            (<div>
+                    <text className={ classNames('NotificationIcon', 'Right') }>Loading</text>
+                    <img className={ classNames('NotificationIcon', 'Left') } src={ loadingIcon }
+                         alt="Loading-icon"/>
+                </div>
+            ) : null }
     </div>;
 }
