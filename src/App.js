9b23ecd93bd7129ab24238f8347bdc7622845873
Sujith PS <sujithps534@gmail.com>
2019-10-10 12:05:13 +0530

    Sujith | Move avatar on click on a table

--- src/App.js
+++ src/App.js
@@ -4,61 +4,74 @@ import Table from './components/Table'
 import Avatar from './components/Avatar'
 import './App.scss';
 
-export const renderHall = () => {
-    const leftStartPoint = 600;
-    const topStartPoint = 720;
-    const tableWidth = 255;
-    const tableHeight = 255;
-    const totalColumns = 5;
-    const totalRows = 3;
+const leftStartPoint = 600;
+const topStartPoint = 720;
+const tableWidth = 255;
+const tableHeight = 255;
+const totalColumns = 5;
+const totalRows = 3;
 
-    const tables = Array.from({ length: totalColumns }, (cItem, cIndex) => {
-        return Array.from({ length: totalRows }, (rItem, rIndex) => {
-                return {
-                    tableLabel: getNumberWithOrdinal((cIndex * totalRows) + rIndex + 1) + " Table",
-                    position: {
-                        top: leftStartPoint + (rIndex * tableHeight) + 'px',
-                        left: topStartPoint + (cIndex * tableWidth) + 'px',
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            avatarPosition: { top: '330px', left: '415px' }
+        };
+    }
+
+    renderHall = () => {
+        const tables = Array.from({ length: totalColumns }, (cItem, cIndex) => {
+            return Array.from({ length: totalRows }, (rItem, rIndex) => {
+                    return {
+                        tableLabel: this.getNumberWithOrdinal((cIndex * totalRows) + rIndex + 1) + " Table",
+                        position: {
+                            top: leftStartPoint + (rIndex * tableHeight) + 'px',
+                            left: topStartPoint + (cIndex * tableWidth) + 'px',
+                        }
                     }
                 }
-            }
-        );
-    });
+            );
+        });
 
-    return tables.flat().map((tableProps, index) => {
-        return (
-            <Table
-                onClick={ null }
-                key={ index }
-                { ...tableProps }
-            />
-        )
-    })
-}
-
-
-function getNumberWithOrdinal(n) {
-    var s = [ "th", "st", "nd", "rd" ],
-        v = n % 100;
-    return n + (s[ (v - 20) % 10 ] || s[ v ] || s[ 0 ]);
-}
-
-export const App = () => {
-    const [ userPosition, setUserPosition ] = React.useState({ top: '330px', left: '415px' })
-
-    return (
-        <div className='Root'>
-            <div className='ConferenceTable'>
-                { renderHall() }
-                <img className='ConferenceImg' src={ conferenceImg } alt="conference hall"/>
-                <Avatar
-                    position={ { top: userPosition.top, left: userPosition.left } }
+        return tables.flat().map((tableProps, index) => {
+            return (
+                <Table
+                    onClick={ null }
+                    key={ index }
+                    moveUser={ this.moveUser }
+                    { ...tableProps }
                 />
+            )
+        })
+    }
+
+    moveUser = (position) => {
+        this.setState({ avatarPosition: position })
+    }
+
+
+    getNumberWithOrdinal = (n) => {
+        var s = [ "th", "st", "nd", "rd" ],
+            v = n % 100;
+        return n + (s[ (v - 20) % 10 ] || s[ v ] || s[ 0 ]);
+    }
+
+    render() {
+        return (
+            <div className='Root'>
+                <div className='ConferenceTable'>
+                    { this.renderHall() }
+                    <img className='ConferenceImg' src={ conferenceImg } alt="conference hall"/>
+                    <Avatar
+                        position={ this.state.avatarPosition }
+                    />
+                </div>
+                <div>
+                </div>
             </div>
-            <div>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -10,13 +10,14 @@ class Table extends React.Component {
         }
     }
 
-    onClick = () => {
+    moveUserToThisTable = () => {
         console.log(this.state.tableLabel)
+        this.props.moveUser(this.props.position)
     }
 
     render = () => {
         return (
-            <div onClick={ this.onClick } className='Root'
+            <div onClick={ this.moveUserToThisTable } className='Root'
                  style={ { top: this.state.position.top, left: this.state.position.left } }>
                 <div className='TableLabel'>{ this.state.tableLabel }</div>
             </div>
