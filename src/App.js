e63f8270710f3b7b1c04a763fffd9c476f1f23dd
Sujith PS <sujithps534@gmail.com>
2019-10-10 11:06:41 +0530

    Sujith | Refactor Table index display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react'
-import Avatar from './components/Avatar'
 import conferenceImg from './assets/images/conference.svg'
 import Table from './components/Table'
 import './App.scss';
 
-export const renderHall = (tableProps, onClick) => {
-    return tableProps.map((tableProps, index) => {
+export const renderHall = () => {
+    const leftStartPoint = 600;
+    const topStartPoint = 720;
+    const tableWidth = 255;
+    const tableHeight = 255;
+    const totalColumns = 5;
+    const totalRows = 3;
+
+    const tables = Array.from({ length: totalColumns }, (cItem, cIndex) => {
+        return Array.from({ length: totalRows }, (rItem, rIndex) => {
+                return {
+                    tableName: getNumberWithOrdinal((cIndex * totalRows) + rIndex + 1) + " Table",
+                    position: {
+                        top: leftStartPoint + (rIndex * tableHeight) + 'px',
+                        left: topStartPoint + (cIndex * tableWidth) + 'px',
+                    }
+                }
+            }
+        );
+    });
+
+    return tables.flat().map((tableProps, index) => {
         return (
             <Table
-                onClick={ onClick }
+                onClick={ null }
                 key={ index }
                 { ...tableProps }
             />
@@ -16,88 +35,19 @@ export const renderHall = (tableProps, onClick) => {
     })
 }
 
+
+function getNumberWithOrdinal(n) {
+    var s = [ "th", "st", "nd", "rd" ],
+        v = n % 100;
+    return n + (s[ (v - 20) % 10 ] || s[ v ] || s[ 0 ]);
+}
+
 export const App = () => {
-    const [ userPosition, setUserPosition ] = React.useState({ top: '330px', left: '415px' })
-    const [ connectionStatus, setConnectionStatus ] = React.useState('STRONG')
-    const [ isCannotConnectCamera, setIsCannotConnectCamera ] = React.useState(false)
-    const [ isSharingScreen, setSharingScreen ] = React.useState(false)
-
-    const tableProps = [
-        {
-            tableName: 'show join notification',
-            position: {
-                top: '600px',
-                left: '720px',
-            }
-
-        },
-        {
-            tableName: 'show left notification',
-            position: {
-                top: '863px',
-                left: '720px',
-            }
-        },
-        {
-            tableName: 'notification when connection low',
-            position: {
-                top: '1125px',
-                left: '720px',
-            }
-
-        },
-        {
-            tableName: 'notification when connection middle',
-            position: {
-                top: '600px',
-                left: '975px',
-            }
-
-        },
-        {
-            tableName: 'When connection strong',
-            position: {
-                top: '863px',
-                left: '975px',
-            }
-
-        },
-        {
-            tableName: 'notification when cannot connect to camera strong',
-            position: {
-                top: '1125px',
-                left: '975px',
-            }
-
-        },
-        {
-            tableName: 'share screen notification',
-            position: {
-                top: '600px',
-                left: '1232px',
-            }
-
-        },
-        {
-            tableName: 'turn off share screen notification',
-            position: {
-                top: '863px',
-                left: '1232px',
-            }
-
-        }
-    ]
-
     return (
         <div className='Root'>
             <div className='ConferenceTable'>
-                { renderHall(tableProps, setUserPosition) }
-                <img className='ConferenceImg' src={ conferenceImg } alt="conference"/>
-                <Avatar
-                    isCannotConnectCamera={ isCannotConnectCamera }
-                    connectionStatus={ connectionStatus }
-                    position={ { top: userPosition.top, left: userPosition.left } }
-                />
+                { renderHall() }
+                <img className='ConferenceImg' src={ conferenceImg } alt="conference hall"/>
             </div>
             <div>
             </div>
